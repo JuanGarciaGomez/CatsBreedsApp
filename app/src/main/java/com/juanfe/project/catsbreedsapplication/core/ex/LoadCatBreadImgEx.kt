a70f4d279f31e91414e4bf5e4c52a59c0a0299f2
Juan Felipe Garcia <70071051+JuanGarciaGomez@users.noreply.github.com>
2024-09-21 12:31:10 -0500

    Add Glide Transformation

--- app/src/main/java/com/juanfe/project/catsbreedsapplication/core/ex/LoadCatBreadImgEx.kt
+++ app/src/main/java/com/juanfe/project/catsbreedsapplication/core/ex/LoadCatBreadImgEx.kt
@@ -2,11 +2,16 @@ package com.juanfe.project.catsbreedsapplication.core.ex
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.juanfe.project.catsbreedsapplication.R
+import com.juanfe.project.catsbreedsapplication.core.CenterCropBlurTransformation
 
 
 fun ImageView.loadCatBreadImg(imgId: String) {
     Glide.with(this).load("https://cdn2.thecatapi.com/images/$imgId.jpg")
         .thumbnail(0.25f)
-        .centerCrop()
+        .placeholder(R.mipmap.ic_launcher_round)
+        .apply(RequestOptions().transform(CenterCropBlurTransformation(this.context, 25)))
+        .sizeMultiplier(0.5f)
         .into(this)
 }
